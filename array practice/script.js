2d58fb147d1e4af899fa9d9626799c4c58be0caf
md-arish-17 <mdarish1801@gmail.com>
2025-07-17 15:23:26 +0530

    code deployed

---
+++ array practice/script.js
@@ -0,0 +1,60 @@
+let array = ["apple","mango","banana","guava"]
+
+// array.length = 2
+// console.log(array.length)
+
+let number = [29,84,51,28,93]
+// let myList = number.toString()
+// console.log(myList)
+// console.log(typeof myList)
+
+// console.log(array.at(3))
+// console.log(number.at(2))
+
+// console.log(array.join("-"))
+// console.log(number.join("_"))
+
+// array.push("arish")
+// console.log(array)
+// number.unshift("umar")
+// console.log(number)
+// array.pop()
+// console.log(array)
+// number.shift()
+// console.log(number)
+
+// array[array.length] = "watermelon"
+// console.log(array)
+
+// delete array[0]
+// delete array[2]
+// console.log(array)
+
+// let newArray = array + number
+// let newArray = array.concat(number)
+// console.log(newArray)
+
+// let vegetables = ["tomato","brinjal","garlic"]
+// newArray = vegetables.concat(number,array)
+// console.log(newArray)
+
+// let myCopy = number.copyWithin(2,2)
+// console.log(myCopy)
+
+// let myArray = [["arish","umar","ayan"],[92,61,48],["bread","butter"]]
+// let flattenArray = myArray.flat()
+// console.log(myArray)
+// console.log(flattenArray)
+
+let fruits = ["guava","grapes","strawberry","cheeko","cashew","walnut"]
+// console.log(fruits)
+// let slicedArray = fruits.slice(1,4)
+// console.log(slicedArray)
+
+// fruits.splice(2,2)
+// fruits.splice(3,0,"boss","hitler")
+// console.log(fruits)
+
+let myList = fruits.toSpliced(1,3)
+console.log(fruits)
+console.log(myList)
